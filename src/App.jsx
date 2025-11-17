101b968cf84143bd94a0f08c2365c90377293b82
Monish <monish@Monishs-MacBook-Pro-2.local>
2025-11-16 22:25:57 -0800

    Second Commit: Initial Prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,85 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MapChart from './Mapchart'
+import LogoSection from './LogoSection'
+import usData from './assets/states-albers-10m.json'
+import ProtestantLabel from './ProtestantLabel'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [displayText, setDisplayText] = useState("");
+  const [index, setIndex] = useState(0);
+  const message = "Hi my name is monish";
+  useEffect(() => {
+    if (index >= message.length) return;
 
+    const timeoutID = setTimeout(() => {
+      setDisplayText((prev) => prev + message[index]);
+      setIndex((pi) => pi + 1);
+    }, 100);
+
+    return () => clearTimeout(timeoutID);
+  }, [index]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className='min-h-screen bg-slate-200 flex justify-center py-10'>
+      <main className='bg-white shadow-lg border-slate-300 
+       w-[794px] min-h-[1123px]
+       rounded-sm px-12 py-16
+      '>
+
+        <LogoSection />
+        <br />
+        <p className='text-[purple] text-2xl font-bold'>Tracing the Spread of Protestantism in the United States</p>
+        <p className=" text-xl text-[purple]">
+          History and Present-Day Footprint
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <br />
+        {/* <h1 className='xl text-black'>{displayText}</h1> */}
+        <section className='text-black font-sans text-justify'>
+          <p className=" text-xl text-[purple]">Introduction</p>
+          <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+          <br />
+          <p className=" text-xl text-[purple]">Historical Overview</p>
+          <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+          <br />
+          <div className='relative'>
+            <div className="origin-top-right scale-75 float-right ml-6">
+              <ProtestantLabel />
+            </div>
+            <div className='text-start'>
+              <ol className='space-y-2 list-decimal pl-4'>
+                <li>1. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li>2. Aliquam tincidunt mauris eu risus.</li>
+                <li>3. Vestibulum auctor dapibus neque.</li>
+              </ol>
+              <br />
+              <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+            </div>
+            <p className='mt-4'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+            <div className='clear-both' />
+          </div>
+
+          <br />
+          <p className=" text-xl text-[purple]">Data and Definitions</p>
+          <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+          <br />
+          <p className='text-xl text-[purple]'>The Religious Landscape Today</p>
+          <br />
+          <MapChart us={usData} />
+          <br />
+          <section>
+            <p className='font-bold'>Citations</p>
+            <p>Pew Research Center - <a className='text-[#00008b]' href="https://www.pewresearch.org/religious-landscape-study/?gad_source=1&gad_campaignid=22378837192&gbraid=0AAAAA-ddO9ENPLuTQ1VMpTLSt_E-sOSXb&gclid=Cj0KCQiA5uDIBhDAARIsAOxj0CGNxYSMKe41sXcLeCBwAEC5uuG9kWKCpcLKa7bD0NM5ej6xyjftuw8aAhIqEALw_wcB">Religious Landscape Study 2023-24</a>
+            </p>
+
+          </section>
+
+        </section>
+      </main>
+
+    </div>
   )
 }
 
---
+++ src/ProtestantLabel.jsx
@@ -0,0 +1,147 @@
+export default function ProtestantLabel() {
+
+    return (<>
+        <div class="min-h-[200px] max-w-[400px] flex-row border-1 px-2">
+            <div class="text-4xl font-extrabold">Protestantism Spread</div>
+            <hr />
+            <div class="text-base font-light">6 Mainstream Denominations</div>
+            <div class="justify-between text-lg font-extrabold">Totals across all 50 States of the USA</div>
+            <hr class="border-t-16 border-black my-1" />
+            <div class="font-extrabold grid grid-cols-2">
+                <div class="col-span-2">Protestants per county</div>
+                <div class="text-4xl
+   font-extrabold col-span-1">Population</div>
+                <div class=" font-extrabold col-span-1 text-4xl text-right pr-2">200</div>
+            </div>
+
+
+            <hr class="border-t-8 border-black my-1" />
+
+            <div class="grid grid-cols-6 gap-2">
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 font-extrabold">Anglicanism</div>
+                    <div class="text-right pr-2">8M</div>
+                    <div class="font-extrabold">10%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Episcopal</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">10%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 font-extrabold">Lutheranism</div>
+                    <div class="text-right pr-2">13M</div>
+                    <div class="font-extrabold">10%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Lutheran</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">LCMS</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">ELCA</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 font-extrabold">Reformed</div>
+                    <div class="col-span-1 text-right pr-2">8M</div>
+                    <div class="col-span-1 font-extrabold">10%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Christian Reformed Church (CRC)</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Reformed Church in America (RCA)</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Congregational (UCC)</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Presbyterian (PC-USA)</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Presbyterian Church in America (PCA)</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 font-extrabold">Methodism</div>
+                    <div class="text-right pr-2">8M</div>
+                    <div class="font-extrabold">10%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">United Methodist</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Free Methodist</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Church of the Nazarene</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 font-extrabold">Baptist</div>
+                    <div class="text-right pr-2">8M</div>
+                    <div class="font-extrabold">10%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">American Baptist</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6 border-b border-gray-300">
+                    <div class="col-span-4 pl-4 text-sm">Southern Baptist</div>
+                    <div class="col-span-1 pl-8">8M</div>
+                    <div class="col-span-1 font-extrabold">33%</div>
+                </div>
+
+                <div class="col-span-6 grid grid-cols-6  border-gray-300">
+                    <div class="col-span-4 font-extrabold">Restoration</div>
+                    <div class="text-right pr-2">8M</div>
+                    <div class="font-extrabold">10%</div>
+                </div>
+            </div>
+            <hr class="border-t-8 border-black my-1" />
+            <div class="text-sm">
+                <div>Acronyms</div>
+                <div class="col-span-2 pl-4">LCMS = Lutheran Church - Missouri Synod</div>
+                <div class="col-span-2 pl-4">ELCA = Evangelical Lutheran Church of America</div>
+            </div>
+        </div>
+    </>);
+}
